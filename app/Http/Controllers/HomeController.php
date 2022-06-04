bacebbcf0d62afed9690275212b82e023856c5d0
mahmoudGeoking2022 <mahmoud.ahmed@geoking.com>
2022-06-04 14:50:06 +0200

    set up notification package

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,16 +4,36 @@ namespace App\Http\Controllers;
 
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Edujugon\PushNotification\PushNotification;
 
 class HomeController extends Controller
 {
-  //
-
   public function test()
   {
-    //test commit  again
-    $newDateTime = Carbon::now()->subYear(2)->format('Y-m-d');
+    $dataIos = [
+      'title' => 'test title ',
+      'body' => 'tset body',
+    ];
 
-    dd($newDateTime);
+    $push = new PushNotification('fcm');
+
+    $push->setMessage([
+      'data' => $dataIos, 'notification' => $dataIos,
+    ])
+      ->setApiKey("AAAAXNdqSzc:APA91bFbgzD6d2oU9SwXOTckUl2Scg6wjgpkSbxWweH0yjVMN0Xzk9BCWUdtmXNOc07ystI20CZMoMhr9nyirdDrixT70sRdAuJaXZQEyayIrUlKnnsrGd12y4zQ8lSBzFnPkNx8jvmj")
+      ->setDevicesToken('test')
+      ->send()
+      ->getFeedback();
+
+    // Notification::create([
+    //   'customer_id' => 1,
+    //   'title' => $data['title'],
+    //   'body' => $data['body'],
+    // ]);
+    // dd($push);
+
+    dd($push);
+    // \Log::info(print_r($push), true);
+    return $push;
   }
 }
