c168db80a167576c9249e496998505ea3749c825
rahmat mondol <rahmat.mondol007@gmail.com>
2024-08-20 02:03:50 +0600

    change main school transportations index page

--- resources/views/school/transportations/index.blade.php
+++ resources/views/school/transportations/index.blade.php
@@ -4,176 +4,128 @@ $page = 'transportations';
 $title = __('site.Transportations');
 ?>
 @section('title_page')
-{{ $title }}
+    {{ $title }}
 @endsection
 @section('content')
+    <!-- BEGIN: Breadcrumb -->
+    <div class="mb-5">
+        <ul class="m-0 p-0 list-none">
+            <li class="inline-block relative top-[3px] text-base text-primary-500 font-Inter ">
+                <a href="{{ route('school.dashboard') }}">
+                    <iconify-icon icon="heroicons-outline:home"></iconify-icon>
+                    <iconify-icon icon="heroicons-outline:chevron-right"
+                        class="relative text-slate-500 text-sm rtl:rotate-180"></iconify-icon>
+                </a>
+            </li>
 
-<!-- Content Wrapper. Contains page content -->
-<div class="content-wrapper">
-  <!-- Content Header (Page header) -->
-  <section class="content-header">
-    <div class="container-fluid">
-      <div class="row mb-2">
-        <div class="col-sm-6">
-          <h6>{{ $title }}
-            <small>
-              ( {{ $transportations->total() }} )
-            </small>
-          </h6>
-
-        </div>
-        <div class="col-sm-6">
-          <ol class="breadcrumb float-sm-right">
-            <li class="breadcrumb-item"><a href="{{ route($mainRoutePrefix.'.dashboard') }}">@lang('site.Home')</a></li>
-            <li class="breadcrumb-item active">{{ $title }}</li>
-          </ol>
-        </div>
-        <div class="col-sm-12">
-
-          <form action="{{ route($mainRoutePrefix.'.transportations.index') }}" method="get">
-
-            <div class="row">
-
-              <div class="col-md-4">
-                <div class="form-group">
-                  <input type="text" name="search" class="form-control" placeholder="@lang('site.search')"
-                    value="{{ request()->search }}">
-                </div>
-              </div>
-
-              <div class="col-md-4">
-                <div class="form-group">
-                  <select name="school_id" class="form-control"  data-live-search="true">
-                    <option value="">@lang('site.Schools') </option>
-                    @foreach( $schools as $scool )
-                    <option value="{{ $scool->id}}" @selected(request('school_id')==$scool->id) >
-                      {{ $scool->title }}</option>
-                    @endforeach
-                  </select>
-                </div>
-              </div>
-
-              <div class="col-md-4">
-                <div class="form-group">
-                  <button type="submit" class="btn btn-sm btn-primary"><i class="fa fa-search"></i>
-                    @lang('site.Search')</button>
-                  @if (checkAdminPermission('create_transportations'))
-                  <a href="{{ route($mainRoutePrefix.'.transportations.create') }}" class="btn btn-sm btn-primary"><i class="fa fa-plus"></i>
-                    @lang('site.Add')</a>
-                  @endif
-                </div>
-            </div>
-
-            </div>
-          </form><!-- end of form -->
-        </div>
-      </div>
-    </div><!-- /.container-fluid -->
-  </section>
-
-  <!-- Main content -->
-  <section class="content">
-
-    <!-- Default box -->
-    <div class="card">
-      <div class="card-header">
-        <h3 class="card-title">{{ $title }}</h3>
-
-        <div class="card-tools">
-          <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
-            <i class="fas fa-minus"></i>
-          </button>
-        </div>
-      </div>
-      <div class="card-body p-0">
-        <table class="table table-striped projects">
-          <thead>
-            <tr>
-              <th style="width: 1%">
-                #
-              </th>
-              <th style="width: 20%">
-                @lang('site.Title')
-              </th>
-              <th style="width: 20%">
-                @lang('site.School')
-              </th>
-              <th style="width: 20%">
-                @lang('site.Price')
-              </th>
-              <th style="width: 8%" >
-                @lang('site.Status')
-              </th>
-              <th style="width: 8%" >
-                @lang('site.table.Order Item')
-              </th>
-              <th style="width: 20%" >
-                @lang('site.Actions')
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            @forelse ($transportations as $value )
-            <tr>
-              <td>
-                {{ $loop->iteration }}
-              </td>
-              <td>
-                {{ $value->title }}
-              </td>
-              <td class="text-center">
-                {{ $value->school?->title }}
-              </td>
-              <td>
-                {{ $value->price }}
-              </td>
-              <td class="project-state">
-                @include('school.partials._render_status',['status'=>$value->status])
-              </td>
-              <td>
-                {{ $value->order_column }}
-              </td>
-              <td class="project-actions text-right">
-
-                @include('school.partials._view_btn',[
-                'txt'=>__('site.View'),
-                'route'=>route($mainRoutePrefix.'.transportations.show', ['transportation'=>$value->id]),
-                'permission' =>'read_transportations',
-                ])
-
-                @include('school.partials._edit_btn',[
-                'txt'=>__('site.Edit') ,
-                'route'=>route($mainRoutePrefix.'.transportations.edit', ['transportation'=>$value->id]),
-                'permission' =>'update_transportations',
-                ])
-
-                @include('school.partials._destroy_btn',[
-                'txt'=>__('site.Delete'),
-                'route'=>route($mainRoutePrefix.'.transportations.destroy', $value->id),
-                'permission' =>'delete_transportations',
-                ])
-
-              </td>
-            </tr>
-            @empty
-            <tr>
-              <td>
-                @include('school.partials.no_data_found')
-              </td>
-            </tr>
-            @endforelse
-
-          </tbody>
-        </table>
-        {{ $transportations->appends(request()->query())->links() }}
-
-      </div>
-      <!-- /.card-body -->
+            <li class="inline-block relative text-sm text-slate-500 font-Inter dark:text-white">
+                {{ $title }}</li>
+        </ul>
     </div>
-    <!-- /.card -->
+    <!-- END: BreadCrumb -->
+    <div class=" space-y-5">
+        <div class="card">
+            <header class=" card-header noborder">
+                <h4 class="card-title"> {{ $title }}
+                </h4>
+                @if (checkAdminPermission('create_transportations'))
+                    <a href="{{ route($mainRoutePrefix.'.transportations.create') }}" class="btn btn-sm btn-primary">
+                        <iconify-icon icon="heroicons:folder-plus"></iconify-icon>
+                        @lang('site.Add')</a>
+                @endif
+            </header>
+            <div class="card-body px-6 pb-6">
+                <div class="overflow-x-auto -mx-6 dashcode-data-table">
+                    <span class=" col-span-8  hidden"></span>
+                    <span class="  col-span-4 hidden"></span>
+                    <div class="inline-block min-w-full align-middle">
+                        <div class="overflow-hidden ">
+                            <table
+                                class="min-w-full divide-y divide-slate-100 table-fixed dark:divide-slate-700 data-table">
+                                <thead class=" bg-slate-200 dark:bg-slate-700">
+                                    <tr>
+                                        <th scope="col" class=" table-th " style="width: 1%">
+                                            Id
+                                        </th>
 
-  </section>
-  <!-- /.content -->
-</div>
-<!-- /.content-wrapper -->
+                                        <th scope="col" class=" table-th " style="width: 20%">
+                                            @lang('site.Title')
+                                        </th>
+
+                                        <th scope="col" class=" table-th " style="width: 20%">
+                                            @lang('site.School')
+                                        </th>
+
+                                        <th scope="col" class=" table-th " style="width: 8%">
+                                            @lang('site.Price')
+                                        </th>
+
+                                        <th scope="col" class=" table-th " style="width: 8%">
+                                            @lang('site.Status')
+                                        </th>
+                                        <th scope="col" class=" table-th " style="width: 8%">
+                                            @lang('site.table.Order Item')
+                                        </th>
+
+                                        <th scope="col" class=" table-th " style="width: 20%">
+                                            @lang('site.Actions')
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody class="bg-white divide-y divide-slate-100 dark:bg-slate-800 dark:divide-slate-700">
+                                    @forelse ($transportations as $value)
+                                        <tr>
+                                            <td class="table-td">{{ $loop->iteration }}</td>
+                                            <td class="table-td ">{{ $value->title }}</td>
+                                            <td class="table-td "> {{ $value->school?->title }}</td>
+                                            <td class="table-td "> {{ $value->price }}</td>
+                                            <td class="table-td ">
+                                                @if ($value->status)
+                                                    <div
+                                                        class="inline-block px-3 min-w-[90px] text-center mx-auto py-1 rounded-[999px] bg-opacity-25 text-success-500 bg-success-500">
+                                                        @lang('site.Active')
+                                                    </div>
+                                                @else
+                                                    <div
+                                                        class="inline-block px-3 min-w-[90px] text-center mx-auto py-1 rounded-[999px] bg-opacity-25 text-danger-500 bg-danger-500">
+                                                        @lang('site.In-Active')
+                                                    </div>
+                                                @endif
+                                            </td>
+                                            <td class="table-td "> {{ $value->order_column }}</td>
+                                            <td class="table-td ">
+                                                <div class="flex space-x-3 rtl:space-x-reverse">
+                                                    <a href="{{ route($mainRoutePrefix . '.transportations.show', ['transportation' => $value->id]) }}"
+                                                        class="action-btn">
+                                                        <iconify-icon icon="heroicons:eye"></iconify-icon>
+                                                    </a>
+                                                    <a href="{{ route($mainRoutePrefix . '.transportations.edit', ['transportation' => $value->id]) }}"
+                                                        class="action-btn">
+                                                        <iconify-icon icon="heroicons:pencil-square"></iconify-icon>
+                                                    </a>
+                                                    @include('school.partials._destroy_btn', [
+                                                        'txt' => __('site.Delete'),
+                                                        'route' => route(
+                                                            $mainRoutePrefix . '.transportations.destroy',
+                                                            $value->id),
+                                                        'permission' => 'delete_transportations',
+                                                    ])
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    @empty
+                                        <tr>
+                                            <td>
+                                                @include('school.partials.no_data_found')
+                                            </td>
+                                        </tr>
+                                    @endforelse
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
