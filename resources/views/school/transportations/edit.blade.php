c543dc137b433c678a0e05405c854d287b5019d6
rahmat mondol <rahmat.mondol007@gmail.com>
2024-08-20 02:24:04 +0600

    change main school transportations edit page

--- resources/views/school/transportations/edit.blade.php
+++ resources/views/school/transportations/edit.blade.php
@@ -4,104 +4,114 @@ $page = 'transportations';
 $title = __('site.Edit Transportation');
 ?>
 @section('title_page')
-{{ $title }}
+    {{ $title }}
 @endsection
 @section('content')
-
-<!-- Content Wrapper. Contains page content -->
-<div class="content-wrapper">
-  <!-- Content Header (Page header) -->
-  <section class="content-header">
-    <div class="container-fluid">
-      <div class="row mb-2">
-        <div class="col-sm-6">
-          <h6>{{ $title }}</h6>
-        </div>
-        <div class="col-sm-6">
-          <ol class="breadcrumb float-sm-right">
-            <li class="breadcrumb-item"><a href="{{ route($mainRoutePrefix.'.dashboard') }}">@lang('site.Home')</a></li>
-            <li class="breadcrumb-item"><a href="{{ route($mainRoutePrefix.'.transportations.index') }}">@lang('site.PaidServices')</a>
+    <!-- BEGIN: Breadcrumb -->
+    <div class="mb-5">
+        <ul class="m-0 p-0 list-none">
+            <li class="inline-block relative top-[3px] text-base text-primary-500 font-Inter ">
+                <a href="{{ route($mainRoutePrefix . '.dashboard') }}">
+                    <iconify-icon icon="heroicons-outline:home"></iconify-icon>
+                    <iconify-icon icon="heroicons-outline:chevron-right"
+                        class="relative text-slate-500 text-sm rtl:rotate-180"></iconify-icon>
+                </a>
             </li>
-            <li class="breadcrumb-item active">{{ $title }}</li>
-          </ol>
-        </div>
-      </div>
-    </div><!-- /.container-fluid -->
-  </section>
+            <li class="inline-block relative text-sm text-slate-500 font-Inter dark:text-white">
+                <a href="{{ route($mainRoutePrefix . '.transportations.index') }}">
+                    @lang('site.PaidServices')
+                    <iconify-icon icon="heroicons-outline:chevron-right"
+                        class="relative text-slate-500 text-sm rtl:rotate-180"></iconify-icon>
+                </a>
+            </li>
+            <li class="inline-block relative text-sm text-slate-500 font-Inter dark:text-white">
+                {{ $title }}</li>
+        </ul>
+    </div>
+    <!-- END: BreadCrumb -->
+    <form method="post" action="{{ route($mainRoutePrefix . '.transportations.update', $transportation->id) }}"
+        enctype="multipart/form-data">
+        @csrf
+        @method('put')
+        @include('school.partials._errors')
 
-  <!-- Main content -->
-  <section class="content">
-    <form method="post" action="{{ route($mainRoutePrefix.'.transportations.update',$transportation->id)}}" enctype="multipart/form-data">
-      @csrf
-      @method('put')
-      @include('school.partials._errors')
-      <input type="hidden" name="school_id" value="{{ $globalSchool->id }}">
-      <div class="row">
-        <div class="col-md-6">
-          <div class="card card-primary">
-            <div class="card-body">
+        <input type="hidden" name="school_id" value="{{ $globalSchool->id }}">
 
-              @foreach (config('translatable.locales') as $key => $locale)
-              <div class="form-group">
-                <label>@lang('site.' . $locale . '.Title')</label>
-                <input required="required" type="text" name="{{ $locale }}[title]" class="form-control"
-                  value="{{ old($locale . '.title',$transportation->translate($locale)->title) }}">
-              </div>
-              <div class="  with-border"></div><br>
-              @endforeach
+        <div class="grid xl:grid-cols-2 grid-cols-1 gap-6">
+            @foreach (LaravelLocalization::getSupportedLocales() as $locale => $properties)
+                <div class="card">
+                    <div class="card-body flex flex-col p-6">
+                        <header
+                            class="flex mb-5 items-center border-b border-slate-100 dark:border-slate-700 pb-5 -mx-6 px-6">
+                            <div class="flex-1">
+                                <div class="card-title text-slate-900 dark:text-white">{{ $properties['name'] }}</div>
+                            </div>
+                        </header>
+                        <div class="card-text h-full space-y-4">
+                            {{-- title --}}
+                            <div class="input-area">
+                                <label class="form-label">@lang('site.' . $locale . '.Title')</label>
+                                <input required="required" name="{{ $locale }}[title]"
+                                    value="{{ old($locale . '.title', $transportation->translate($locale)->title) }}"
+                                    type="text" class="form-control">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+            <div class="card">
+                <div class="card-body flex flex-col p-6">
+                    <div class="card-text h-full space-y-4">
+
+                        {{-- price --}}
+                        <div class="input-area">
+                            <label>@lang('site.Price')</label>
+                            <input type="text" name="price" value="{{ old('price', $transportation->price) }}"
+                                class="form-control mt-2"
+                                oninput="this.value = this.value.replace(/[^0-9.]/g, ''); this.value = this.value.replace(/(\..*)\./g, '$1');">
+                        </div>
+
+                        {{-- order_column --}}
+                        <div class="input-area">
+                            <label>@lang('site.Order Item')</label>
+                            <input type="text" name="order_column"
+                                value="{{ old('order_column', $transportation->order_column) }}" class="form-control mt-2"
+                                oninput="this.value = this.value.replace(/[^0-9.]/g, ''); this.value = this.value.replace(/(\..*)\./g, '$1');">
+                        </div>
+
+                        {{-- status --}}
+                        <div class="input-area">
+                            <label for="inputStatus">@lang('site.Status')</label>
+                            <select id="inputStatus" name="status" required class="form-control mt-2">
+                                <option value='' selected disabled>@lang('site.Status')</option>
+                                <option value="1" @if (old('status', $transportation->status) == 1) selected @endif>@lang('site.Active')
+                                </option>
+                                <option value="0" @if (old('status', $transportation->status) == 0) selected @endif>@lang('site.In-Active')
+                                </option>
+                            </select>
+                        </div>
+
+                    </div>
+                </div>
             </div>
-            <!-- /.card-body -->
-          </div>
-          <!-- /.card -->
+
         </div>
-        <div class="col-md-6">
-          <div class="card card-primary">
-            <div class="card-body">
-
-
-              {{-- price --}}
-              <div class="form-group">
-                <label>@lang('site.Price')</label>
-                <input type="text" name="price" value="{{ old('price',$transportation->price) }}"
-                  class="form-control"
-                  oninput="this.value = this.value.replace(/[^0-9.]/g, ''); this.value = this.value.replace(/(\..*)\./g, '$1');">
-              </div>
-
-              {{-- order_column --}}
-              <div class="form-group">
-                <label>@lang('site.Order Item')</label>
-                <input type="text" name="order_column" value="{{ old('order_column',$transportation->order_column) }}"
-                  class="form-control"
-                  oninput="this.value = this.value.replace(/[^0-9.]/g, ''); this.value = this.value.replace(/(\..*)\./g, '$1');">
-              </div>
-
-              {{-- status --}}
-              <div class="form-group">
-                <label for="inputStatus">@lang('site.Status')</label>
-                <select id="inputStatus" name="status" required class="form-control custom-select">
-                  <option value='' selected disabled>@lang('site.Status')</option>
-                  <option value="1" @if(old('status',$transportation->status)==1) selected @endif>@lang('site.Active')</option>
-                  <option value="0" @if(old('status',$transportation->status)==0) selected @endif>@lang('site.In-Active')
-                  </option>
-                </select>
-              </div>
-
+        {{-- save --}}
+        <div class="grid xl:grid-cols-1 grid-cols-1 gap-6 mt-6">
+            <div class="card rounded-md bg-white dark:bg-slate-800 lg:h-full shadow-base">
+                <div class="card-body flex flex-col p-6">
+                    <div class="card-text h-full space-y-6">
+                        <div class="input-area text-center">
+                            <button class="btn inline-flex justify-center btn-primary" type="submit" name='continue'
+                                value='continue'><i class="fas fa-save"></i>
+                                @lang('site.Save & Continue')</button>
+                            <button class="btn inline-flex justify-center btn-primary" type="submit"><i
+                                    class="fas fa-save"></i>
+                                @lang('site.Save')</button>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <!-- /.card-body -->
-          </div>
-          <!-- /.card -->
         </div>
-      </div>
-      <div class="row">
-        <div class="col-12">
-          <button class="btn btn-success" type="submit" name='continue' value='continue'><i class="fas fa-save"></i>
-            @lang('site.Save & Continue')</button>
-          <button class="btn btn-success" type="submit"><i class="fas fa-save"></i> @lang('site.Save')</button>
-        </div>
-      </div>
     </form>
-  </section>
-  <!-- /.content -->
-</div>
-<!-- /.content-wrapper -->
 @endsection
