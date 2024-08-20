7df789e8369eeae7a1b1f51a177b91135fa43555
rahmat mondol <rahmat.mondol007@gmail.com>
2024-08-20 14:33:55 +0600

    change main school attachments show page

--- resources/views/school/attachments/show.blade.php
+++ resources/views/school/attachments/show.blade.php
@@ -7,52 +7,90 @@ $title = __('site.Show Attachment');
 {{ $title }}
 @endsection
 @section('content')
-
-<!-- Content Wrapper. Contains page content -->
-<div class="content-wrapper">
-  <!-- Content Header (Page header) -->
-  <section class="content-header">
-    <div class="container-fluid">
-      <div class="row mb-2">
-        <div class="col-sm-6">
-          <h6>{{ $title }}</h6>
-        </div>
-        <div class="col-sm-6">
-          <ol class="breadcrumb float-sm-right">
-            <li class="breadcrumb-item"><a href="{{ route($mainRoutePrefix.'.dashboard') }}">@lang('site.Home')</a></li>
-            <li class="breadcrumb-item"><a href="{{ route($mainRoutePrefix.'.attachments.index') }}">@lang('site.Attachments')</a>
+    <!-- Content Wrapper. Contains page content -->
+    <!-- BEGIN: Breadcrumb -->
+    <div class="mb-5">
+        <ul class="m-0 p-0 list-none">
+            <li class="inline-block relative top-[3px] text-base text-primary-500 font-Inter ">
+                <a href="index.html">
+                    <iconify-icon icon="heroicons-outline:home"></iconify-icon>
+                    <iconify-icon icon="heroicons-outline:chevron-right"
+                        class="relative text-slate-500 text-sm rtl:rotate-180"></iconify-icon>
+                </a>
             </li>
-            <li class="breadcrumb-item active">{{ $title }}</li>
-          </ol>
-        </div>
-      </div>
-    </div><!-- /.container-fluid -->
-  </section>
-
-  <!-- Content -->
-  <div class="card mt-4 content-table">
-    <div class="card-body">
-      <div class="table-responsive">
-        <table class="table table-striped table-bordered">
-          <tbody>
-            <tr>
-              <td>@lang('site.Title')</td>
-              <td>{{ $attachment->title }}</td>
-            </tr>
-            <tr>
-              <td>@lang('site.Order Item')</td>
-              <td>{{ $attachment->order_column }}</td>
-            </tr>
-            <tr>
-              <td>@lang('site.Status')</td>
-              <td>@include('school.partials._render_status',['status'=>$attachment->status])</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+            <li class="inline-block relative text-sm text-slate-500 font-Inter dark:text-white">
+                <a href="{{ route($mainRoutePrefix.'.attachments.index') }}">
+                  @lang('site.Attachments')
+                    <iconify-icon icon="heroicons-outline:chevron-right"
+                        class="relative text-slate-500 text-sm rtl:rotate-180"></iconify-icon>
+                </a>
+            </li>
+            <li class="inline-block relative text-sm text-slate-500 font-Inter dark:text-white">
+                {{ $title }}</li>
+        </ul>
     </div>
-  </div>
-  <!-- //Content -->
-</div>
-<!-- /.content-wrapper -->
+    <!-- END: BreadCrumb -->
+
+
+    <div class="grid xl:grid-cols-1 grid-cols-1 gap-5">
+        <!-- BEGIN: Bordered Table -->
+        <div class="card">
+            <header class=" card-header noborder">
+                <h4 class="card-title">
+                    {{ $title }}
+                </h4>
+            </header>
+            <div class="card-body px-6 pb-6">
+                <div class="overflow-x-auto ">
+                    <div class="inline-block min-w-full align-middle">
+                        <div class="overflow-hidden ">
+                            <table class="min-w-full divide-y divide-slate-100 table-fixed dark:divide-slate-700">
+                                <tbody class="bg-white divide-y divide-slate-100 dark:bg-slate-800 dark:divide-slate-700">
+
+                                    <tr>
+                                        <td style="width: 40%;"
+                                            class="table-td border border-slate-100 dark:bg-slate-800 dark:border-slate-700">
+                                            @lang('site.Title')</td>
+                                        <td
+                                            class="table-td border border-slate-100 dark:bg-slate-800 dark:border-slate-700">
+                                            {{ $attachment->title }}</td>
+                                    </tr>
+
+                                    <tr>
+                                        <td
+                                            class="table-td border border-slate-100 dark:bg-slate-800 dark:border-slate-700">
+                                            @lang('site.Order Item')</td>
+                                        <td
+                                            class="table-td border border-slate-100 dark:bg-slate-800 dark:border-slate-700">
+                                            {{ $attachment->order_column }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td
+                                            class="table-td border border-slate-100 dark:bg-slate-800 dark:border-slate-700">
+                                            @lang('site.Status')</td>
+                                        <td
+                                            class="table-td border border-slate-100 dark:bg-slate-800 dark:border-slate-700">
+                                            @if ($attachment->status)
+                                                <div
+                                                    class="inline-block px-3 min-w-[90px] text-center mx-auto py-1 rounded-[999px] bg-opacity-25 text-success-500 bg-success-500">
+                                                    @lang('site.Active')
+                                                </div>
+                                            @else
+                                                <div
+                                                    class="inline-block px-3 min-w-[90px] text-center mx-auto py-1 rounded-[999px] bg-opacity-25 text-danger-500 bg-danger-500">
+                                                    @lang('site.In-Active')
+                                                </div>
+                                            @endif
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- END: Bordered Table -->
+    </div>
+    <!-- /.content-wrapper -->
 @endsection
