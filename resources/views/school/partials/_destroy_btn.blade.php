17bb5a2eb5d96d86db37f28fcf5b3d5ad329918d
rahmat mondol <rahmat.mondol007@gmail.com>
2024-08-20 20:36:22 +0600

    change main school change discount index

--- resources/views/school/partials/_destroy_btn.blade.php
+++ resources/views/school/partials/_destroy_btn.blade.php
@@ -1,9 +1,9 @@
 @if (checkAdminPermission('delete_roles'))
-<form action="{{ $route }}" method="post" style="display: inline-block">
-  @csrf
-  @method('delete')
-  <button type="submit" class="action-btn">
-    <iconify-icon icon="heroicons:trash"></iconify-icon>
-  </button>
-</form><!-- end of form -->
+    <form action="{{ $route }}" method="post" style="display: inline-block">
+        @csrf
+        @method('delete')
+        <button type="submit" class="action-btn">
+            <iconify-icon icon="heroicons:trash"></iconify-icon>
+        </button>
+    </form><!-- end of form -->
 @endif
